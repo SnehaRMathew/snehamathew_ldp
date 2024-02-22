c1223a5b332a37b64f3fdbfae23aca7f7b1f1754
SnehaRMathew <sneha.mathew@zemosolabs.com>
2024-02-22 18:33:09 +0530

    Adding the project to git

---
+++ src/main/java/com/zemoso/checkr/CheckrApplication.java
@@ -0,0 +1,13 @@
+package com.zemoso.checkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckrApplication.class, args);
+	}
+
+}
